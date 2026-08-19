c4851aaa37101ecac046e1879614ea65d415e800
Binnnnn02 <verybintang548@gmail.com>
2026-08-19 20:53:20 +0700

    Mengganti nama bank dan nama pemilik akun bank

--- client/src/lib/subscriptionPlan.js
+++ client/src/lib/subscriptionPlan.js
@@ -4,12 +4,12 @@
 // supaya harga/rekening/nomor WA cuma perlu diubah di satu tempat.
 // ============================================================
 
-export const PLAN_PRICE = "Rp35.999"; // TODO: ganti harga langganan per bulan
+export const PLAN_PRICE = "Rp30.000"; // TODO: ganti harga langganan per bulan
 export const PLAN_DURATION = "30 hari";
 
-export const BANK_NAME = "BCA"; // TODO: ganti nama bank/e-wallet
+export const BANK_NAME = "PANIN BANK"; // TODO: ganti nama bank/e-wallet
 export const BANK_ACCOUNT_NUMBER = "1234567890"; // TODO: ganti nomor rekening
-export const BANK_ACCOUNT_NAME = "Nama Pemilik Rekening"; // TODO: ganti nama pemilik
+export const BANK_ACCOUNT_NAME = "Bintang Very Purwanto"; // TODO: ganti nama pemilik
 
 export const WHATSAPP_NUMBER = "62895629208339"; // TODO: ganti nomor WA (format 62xxxxxxxxxx, tanpa + atau spasi)
 
